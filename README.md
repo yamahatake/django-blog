07471b28ba8edd8aad1abf59b8f0af7213e76d8a
Augusto.yamamoto <augusto.yamamoto@fullbar.com.br>
2019-07-12 19:43:01 -0300

    blog working

--- README.md
+++ README.md
@@ -2,3 +2,33 @@
 ```
 Start
 ```
+
+## Useful commands
+```
+Install virtual enviroment
+- pip install virtualenv 
+
+Create virtual enviroment
+- python -m venv myvenv
+
+Activate virtual enviroment
+- myvenv\scripts\activate
+
+Install Django
+- pip install django
+
+Start Django project
+- django-admin startproject projectname
+
+Create app
+- python manage.py startapp appname
+
+Migrate (queue changes)
+- python manage.py migrate
+
+Migration (apply changes)
+- python manage.py makemigrations
+
+Create superuser
+- python mange.py createsuperuser
+```
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'postgres',
+        'PASSWORD': 'K@kashi123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
